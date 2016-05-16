46c34b6b04fa125d2743b0ae60bd6876349ca108
Erik van der Kolk <developer@smerik.nl>
2016-05-16 22:45:48 +0200

    Replace "this program" in the copyright permission statement
    The GNU GPL howto (http://www.gnu.org/licenses/gpl-howto.en.html) recommends to
    replace "this program" with the name of the program for programs that are more
    than one file.

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 Utility Usage Collector - A utility usage data collecting application.
 Copyright (C) 2015-2016  Erik van der Kolk
 
-This program is free software: you can redistribute it and/or modify
+This file is part of Utility Usage Collector.
+
+Utility Usage Collector is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+Utility Usage Collector is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with Utility Usage Collector.  If not, see <http://www.gnu.org/licenses/>.
 */
 var util = require('util'),
   split = require('split2'),
